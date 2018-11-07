8b84d609a645ab274bccdb9c4e963f87092ba02e
nhoughto <nhoughto@gmail.com>
2018-11-08 09:59:36 +1100

    Fix image push

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@
 ./gradlew clean build shadow
 docker build . -t bsycorp/kees/init:$TRAVIS_BRANCH -f docker/Dockerfile.init
 docker build . -t bsycorp/kees/creator:$TRAVIS_BRANCH -f docker/Dockerfile.creator
-docker push bsycorp/kees
+docker push bsycorp/kees/init
+docker push bsycorp/kees/creator
