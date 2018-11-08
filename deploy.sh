dc3f6d8e87ddf390069f105c182a8bf12b8fa8ca
nhoughto <nhoughto@gmail.com>
2018-11-08 11:04:06 +1100

    Fix docker login

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 ./gradlew clean build shadow
 docker build . -t bsycorp/kees/init:$TRAVIS_BRANCH -f docker/Dockerfile.init
 docker build . -t bsycorp/kees/creator:$TRAVIS_BRANCH -f docker/Dockerfile.creator
+docker login -u $DOCKERUSER -p $DOCKERPASS
 docker push bsycorp/kees/init
 docker push bsycorp/kees/creator
