d7cac8051954c86d2096c12d812252856ee7549e
Nick Houghton <nhoughto@gmail.com>
2019-02-20 15:17:57 +1100

    Use keyutil instead of openssl

--- docker/certs.sh
+++ docker/certs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p /etc/ssl/certs
 echo "" > /etc/ssl/certs/dummy-instance.pem
-(cd /etc/ssl/certs; for file in *${CERT_FILTER:-instance}*.pem; do openssl x509 -outform der -in "$file" -out /tmp/certificate.der | true; keytool -import -alias "$file" -keystore $JAVA_HOME/lib/security/cacerts -file /tmp/certificate.der -deststorepass changeit -noprompt | true ; done);
+java -jar keyutil.jar -e /etc/ssl/certs/*instance*.pem -F -f $JAVA_HOME/lib/security/cacerts -i -p changeit -d
