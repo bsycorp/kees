6af462b5b6fceac049eb610acf9b21b87510d54c
Nick Houghton <nhoughto@gmail.com>
2019-02-20 15:40:45 +1100

    Make cert generation an entrypoint

--- docker/certs.sh
+++ docker/certs.sh
@@ -2,3 +2,4 @@
 mkdir -p /etc/ssl/certs
 echo "" > /etc/ssl/certs/dummy-instance.pem
 java -jar keyutil.jar -e /etc/ssl/certs/*instance*.pem -F -f $JAVA_HOME/lib/security/cacerts -i -p changeit -d
+exec $@
