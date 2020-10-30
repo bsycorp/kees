fdcb869f8fdd4081b961ed14c116df13aab34564
Nick Houghton <nhoughto@gmail.com>
2020-10-31 09:00:47 +1100

    Fix docker package name

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ fi
 ./gradlew build bintrayUpload -i -PappVersion="$TRAVIS_BRANCH" --stacktrace
 ./gradlew nativeImage -i -PappVersion="$TRAVIS_BRANCH" --stacktrace
 (cd init; docker build . -t bsycorp/kees-init:"$TRAVIS_BRANCH")
-(cd create; docker build . -t bsycorp/kees-create:"$TRAVIS_BRANCH")
+(cd create; docker build . -t bsycorp/kees-creator:"$TRAVIS_BRANCH")
 docker push bsycorp/kees-init
-docker push bsycorp/kees-create
+docker push bsycorp/kees-creator
