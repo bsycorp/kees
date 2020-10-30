17da92bb7c978aa285d9efa8c4bd70cf12264b51
Nick Houghton <nhoughto@gmail.com>
2020-10-30 17:26:12 +1100

    Fix github actions

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-export BINTRAY_USER=${{ secrets.BINTRAY_USER }}
-export BINTRAY_KEY=${{ secrets.BINTRAY_KEY }}
 
 if [ -z "$TRAVIS_BRANCH" ]; then
   TRAVIS_BRANCH="${GITHUB_REF##*/}"
@@ -10,6 +8,5 @@ fi
 ./gradlew nativeImage -i -PappVersion="$TRAVIS_BRANCH" --stacktrace
 (cd init; docker build . -t bsycorp/kees-init:"$TRAVIS_BRANCH")
 (cd create; docker build . -t bsycorp/kees-create:"$TRAVIS_BRANCH")
-echo ${{ secrets.DOCKERPASS }} | docker login -u ${{ secrets.DOCKERUSER }} --password-stdin
 docker push bsycorp/kees-init
 docker push bsycorp/kees-create
