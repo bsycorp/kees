a0f612bb27b23ee36d9bf9f47de53e1ceac91140
Nick Houghton <nhoughto@gmail.com>
2021-03-29 17:48:19 +1100

    Use JRE distribution for creator

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ if [ -z "$TRAVIS_BRANCH" ]; then
 fi
 ./gradlew build bintrayUpload -i -PappVersion="$TRAVIS_BRANCH" --stacktrace
 ./gradlew nativeImage -i -PappVersion="$TRAVIS_BRANCH" --stacktrace
+./gradlew distTar -i --stacktrace
 (cd create; docker build . -t bsycorp/kees-creator:"$TRAVIS_BRANCH")
 docker push bsycorp/kees-creator:$TRAVIS_BRANCH
 (cd init; docker build . -t bsycorp/kees-init:"$TRAVIS_BRANCH")
