b465018bc9709c9f011a14e94743eccdc37e49b8
Blair Strang <blair.strang@auspost.com.au>
2021-05-19 13:34:24 +1000

    Do the needful for java version of init && JAVA_OPTS

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,8 @@ fi
 ./gradlew nativeImage -i -PappVersion="$TRAVIS_BRANCH" --stacktrace
 ./gradlew distTar -i --stacktrace
 (cd create; docker build . -t bsycorp/kees-creator:"$TRAVIS_BRANCH")
-docker push bsycorp/kees-creator:$TRAVIS_BRANCH
-(cd init; docker build . -t bsycorp/kees-init:"$TRAVIS_BRANCH")
-docker push bsycorp/kees-init:$TRAVIS_BRANCH
+docker push bsycorp/kees-creator:"$TRAVIS_BRANCH"
+(cd init; docker build . -f Dockerfile.native -t bsycorp/kees-init:"$TRAVIS_BRANCH")
+docker push bsycorp/kees-init:"$TRAVIS_BRANCH"
+(cd init; docker build . -t bsycorp/kees-init:"${TRAVIS_BRANCH}-java")
+docker push bsycorp/kees-init:"${TRAVIS_BRANCH}-java"
